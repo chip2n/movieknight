7e95c08ae2f5c91ba881f93a1fcb6517afc91908
Andreas Arvidsson <andreas.arvidson@gmail.com>
2020-11-22 10:20:01 +0100

    Refactor search dropdown into separate component

--- frontend/src/app/search/views.cljs
+++ frontend/src/app/search/views.cljs
@@ -48,6 +48,27 @@
     (swap! state assoc :input value)
     (rf/dispatch [:search value])))
 
+(defn- handle-click [result]
+  (rf/dispatch [:suggest-movie (:id result)]))
+
+(defn- search-dropdown [results selected-index]
+  [:ul {:style {:position :absolute
+                :width "100%"
+                :margin 0
+                :top 32
+                :padding 0
+                :list-style :none
+                :box-shadow "0px 8px 8px 0px rgba(0,0,0,0.1)"
+                :z-index 1
+                :background-color "#ffffff"}}
+   (for [[result i] (map vector results (range))]
+     ^{:key (str "search-result-" (:id result))}
+     [:li.dropdown-content {:style {:background-color
+                                    (if (= i selected-index)
+                                      "#ff0000")}
+                            :on-click (partial handle-click result)}
+      (:label result)])])
+
 (defn search-bar []
   (let [state (r/atom (initial-state))
         ref (clojure.core/atom nil)
@@ -76,20 +97,4 @@
                     :on-key-down (partial handle-keydown state search-results)
                     :on-change (partial handle-change state)}]
            (when (and expanded search-results)
-             [:ul {:style {:position :absolute
-                           :width "100%"
-                           :margin 0
-                           :top 32
-                           :padding 0
-                           :list-style :none
-                           :box-shadow "0px 8px 8px 0px rgba(0,0,0,0.1)"
-                           :z-index 1
-                           :background-color "#ffffff"}}
-              (for [[result i] (map vector search-results (range))]
-                ^{:key (str "search-result-" (:id result))}
-                [:li.dropdown-content
-                 {:style {:background-color
-                          (if (= i selected-index)
-                            "#ff0000")}
-                  :on-click (fn [] (rf/dispatch [:suggest-movie (:id result)]))}
-                 (:label result)])])]))})))
+             [search-dropdown search-results selected-index])]))})))
