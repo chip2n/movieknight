e50037ee621efedafadf9d2ec395b4d8c32241d7
Andreas Arvidsson <andreas.arvidson@gmail.com>
2020-12-06 08:04:44 +0100

    Require db namespace in dev namespace

--- dev/dev.clj
+++ dev/dev.clj
@@ -1,6 +1,7 @@
 (ns dev
   (:require
    [backend.system :as system]
+   [backend.db :as db]
    [com.stuartsierra.component :as component]
    [com.stuartsierra.component.repl
     :refer [reset set-init start stop system]]))
